ee7b2f919d1fdd278b3417ad4ad7210bf834b54f
Whitney Sam <whitneysam@Whitneys-MacBook-Pro.local>
2019-06-13 13:35:01 -0700

    improved html skeleton, established fetch link between client and api

---
+++ scripts/api.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const api = function() {
+  const baseURL = 'https://thinkful-list-api.herokuapp.com/whitneywallace/bookmarks';
+
+  const bookmarkApiFetch = function(url, options) {
+    let error;
+    return fetch(url)
+      .then(response => {
+        // check if our resopnse is NOT ok
+        if (!response.ok) {
+          // if our response is NOT ok, create an error object in our error variable with the error status
+          error = {code: response.status};
+          //check if the content-type of our response is NOT JSON
+          if (!response.headers.get('content-type').includes('json')) {
+          // if the content-type is NOT JSON, add to our error object the status text
+            error.message = response.statusText;
+            // return that our promise rejected with the error object
+            return Promise.reject(error);
+          }
+        }
+        // if our response is OK, return JSON
+        return response.json();
+      })
+      .then(jsonObj => {
+        // NEED MORE CLARIFICATION ON WHAT THIS PORTION DOES
+        if (error) {
+          error.message = jsonObj.message;
+          return Promise.reject(error);
+        }
+        // if there are no errors, return the JSON object (data retrieved)
+        //console.log(jsonObj);
+        return jsonObj;
+      });
+  };
+
+  const getBookmarks = function() {
+    // call our bookmarkApiFetch function using the baseURL
+    // expect to return all bookmark objects
+    return bookmarkApiFetch(baseURL);
+  };
+
+  return {
+    getBookmarks
+  };
+
+}();
